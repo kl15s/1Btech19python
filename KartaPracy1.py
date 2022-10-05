e6c444de2647a2a7dd67de4fd85874a86a7c3465
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-05 12:12:41 +0000

    skończona karta pracy 1

--- KartaPracy1.py
+++ KartaPracy1.py
@@ -1,11 +1,39 @@
 # Zad1
 
-a=int(input())
-b=int(input())
+# a=int(input())
+# b=int(input())
 
-print(a**2+b**2)
+# print(a**2+b**2)
 
 #zad2
-a=int(input())
-b=int(input())
-print((a+b)**2)
+# a=int(input())
+# b=int(input())
+# print((a+b)**2)
+
+#zad3
+# a=int(input())
+# b=int(input())
+# print((a-b)**3)
+
+#Zad4
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# print(a*b*c)
+
+#Zad5
+# a=int(input())
+# b=int(input())
+# print(2*(a+b)/5)
+
+#zad6
+# brutto= float(input())
+# print(brutto / 1.23)
+
+#zad7
+a,b = int(input()), int(input())
+print(a%b)
+
+
+
+
