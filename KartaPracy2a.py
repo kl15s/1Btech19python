95e09b2cf0d611ef8ca60e2bbd3cbb26a227b09d
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-15 11:43:22 +0000

    Zadanie domowe(KartaPracy2a)

---
+++ KartaPracy2a.py
@@ -0,0 +1,23 @@
+#zad1
+# a= int(input())
+# b= int(input())
+
+# if (a+b) % 2 == 0:
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#zad2
+# a= int(input())
+# g= int(input())
+
+# if (a+g)/2 > (a**g)/(1/2)
+#   print("TAK")
+# else:
+#   print("NIE")
+  
+  
+  
+
+
+
