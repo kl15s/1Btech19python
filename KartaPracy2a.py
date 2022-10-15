ad2360ff3c4b724b7bbc7afe5d4f501c97037ddb
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-15 12:21:49 +0000

    Zadanie domowe zostało zadani 3,6

--- KartaPracy2a.py
+++ KartaPracy2a.py
@@ -11,10 +11,46 @@
 # a= int(input())
 # g= int(input())
 
-# if (a+g)/2 > (a**g)/(1/2)
+# if (a+g)/2 > (a**g)/(1/2):
 #   print("TAK")
 # else:
 #   print("NIE")
+
+#zad3
+k= int(input())
+l= int(input())
+m= int(input())
+
+if k-l==0 or l-k==0 or l-m==0 or m-l==0 or k-m==0 or m-k==0:
+  print("TAK")
+else:
+  print("NIE")
+
+
+#zad4
+# a= int(input())
+# b= int(input())
+# c= int(input())
+# d= int(input())
+
+# print(min(a,b,c,d))
+
+#zad5
+# a= int(input())
+# b= int(input())
+# c= int(input())
+
+# if a<(b+c) or b<(a+c) or c<(b+a):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#zad6
+a= int(input())
+b= int(input())
+c= int(input())
+
+
   
   
   
