d6b1b8e938cf6f383bb2076216b1762550922214
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-17 10:20:08 +0000

    zaczęto KartaPracy3

---
+++ KartaPracy3.py
@@ -0,0 +1,20 @@
+#PRE
+# print (list(range(2,8,1)))
+# print(list(range(8,1,-1)))
+# for i in range(3,10):
+#   print(i)
+
+
+# pętla liczb dwucyfrowych od 10 do 21
+
+for i in range(10,22):
+  print(i)
+
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31
+for i in range(15,33,2):
+  print(i)
+  
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# pętla liczb 3-cyfrowych podzielnych przez 20
---
+++ temp.py
@@ -0,0 +1,7 @@
+a= int(input())
+g= int(input())
+
+if (a*g)/2>(1/2):
+  print("TAK")
+else:
+  print("NIE")
