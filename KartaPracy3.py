ff44d17b9943493a890ccba8aac0006744f7d337
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-19 12:15:31 +0000

    zdadanie 1,2 KartaPracy3

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -6,15 +6,37 @@
 
 
 # pętla liczb dwucyfrowych od 10 do 21
-
-for i in range(10,22):
-  print(i)
+# for i in range(10,22):
+#   print(i)
 
 # pętla liczb dwucyfrowych nieparzystych od 15 do 31
-for i in range(15,33,2):
-  print(i)
+# for i in range(15,33,2):
+  # print(i, end=" ")
   
 # pętla liczb dwucyfrowych malejąco (step ujemny)
+# for i in range(99,9,-1):
+#   print(i,end=" ")
 
 # pętla liczb dwucyfrowych malejąco (step dodatni)
+# for i in range(10,100,1):
+#   print(109-i,end=" ")
+  
 # pętla liczb 3-cyfrowych podzielnych przez 20
+# for i in range(100,1000,20):
+#   print(i,end=" ")
+
+#zad1
+# n= int(input())
+# for i in range(n):
+#   print(i**3+3,end=" ")
+
+#zad2
+# for i in range(105,1000,15):
+#   print(i,end=" ")
+
+for i in range (100,1000):
+  if i %15==0
+  print(i,end=" ")
+
+  
+  
--- temp.py
+++
@@ -1,7 +0,0 @@
-a= int(input())
-g= int(input())
-
-if (a*g)/2>(1/2):
-  print("TAK")
-else:
-  print("NIE")
