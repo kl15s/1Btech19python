72f639e29bc1bbe8c1fb35c474480cc33ab0b221
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-24 10:20:20 +0000

    zrobiono zadanie3,4 zaczęto 5

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -22,8 +22,8 @@
 #   print(109-i,end=" ")
   
 #pętla liczb 3-cyfrowych podzielnych przez 20
-for i in range(100,1000,20):
-  print(i,end=" ")
+# for i in range(100,1000,20):
+#   print(i,end=" ")
 
 #zad1
 # n= int(input())
@@ -38,5 +38,26 @@ for i in range(100,1000,20):
 #   if i %15==0
 #   print(i,end=" ")
 
-  
-  
+#zad3
+# n=int(input())
+# for i in range(1,n+1):
+#   if n%i==0:
+#     print(i, end=" ")
+
+#dzielniki właściwe
+# n=int(input())
+# for i in range(2,n):
+#   if n%i==0:
+#     print(i, end=" ")
+
+#zad4
+# s=0
+# for i in range(10,100):
+#   s=s+i
+# print(s)
+
+#zad5
+n=int(input())
+
+for i in range(n-1):
+  k=int(input())
