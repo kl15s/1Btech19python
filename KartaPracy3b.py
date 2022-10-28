b7d4af170ece6dc8d0c98232a9afc0046852556c
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-10-28 18:48:10 +0000

    kombinowanie z KartaPracy3b

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -3,7 +3,6 @@
 #   print(i, end=" ")
 
 #zad2
-
 #zad3
 # for i in range(1516,10000,379):
 #   print(i, end=" ")
@@ -19,5 +18,19 @@
 #   print(m, end=" ")
 
 #zad5
+# n=int(input("Ile liczb chcesz podać?"))
+# suma=0
+# for i in range(n):
+#   print(i, suma)
 
 #zad6
+n=int(input("Ile liczb chcesz do siebie dodać?"))
+liczba=1
+suma=0
+
+
+#zad7
+# m=int(input()
+
+# for i in range(11,100,2):
+  
