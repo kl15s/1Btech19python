16a1efba00de755fa3de7ecd958effe9daad4217
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-11-01 18:49:40 +0000

    ukończono zadanie domowe KartaPracy3b

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -3,6 +3,9 @@
 #   print(i, end=" ")
 
 #zad2
+# for i in range(1,10,2):
+#   print(i**2, end=" ")
+
 #zad3
 # for i in range(1516,10000,379):
 #   print(i, end=" ")
@@ -20,17 +23,60 @@
 #zad5
 # n=int(input("Ile liczb chcesz podać?"))
 # suma=0
+
 # for i in range(n):
-#   print(i, suma)
+#   y=int(input())
+#   suma=suma+y
+#   print(suma)
 
 #zad6
-n=int(input("Ile liczb chcesz do siebie dodać?"))
-liczba=1
-suma=0
+# k=int(input())
+# a=0
 
+# for i in range(0,2*k,2):
+#   a=a+i
+#   print("Suma początkowych liczb parzystychv wynosi:")
+#   print(a)
 
 #zad7
-# m=int(input()
+# m=int(input())
+# a=0
 
-# for i in range(11,100,2):
-  
+# for i in range(11,(m*2)+11,2):
+#   a=a+i
+#   print("Suma liczb początkowych nieparzystych wynosi:")
+#   print(a)
+
+#zad8
+# W0=int(input("Podaj wartość początkową inwestycji:")) 
+# L=int(input("Podaj lata inwestycji:"))
+# Wk=0
+# suma=Wk
+
+# for i in range(0,L * 12):
+#   Wk=suma*0.06*(1/12)
+#   suma= suma+Wk
+#   print("Końcowa wartość inwestycji wynosi:")
+#   print(suma)
+
+#zad9
+# n=int(input("Podaj ilość liczb:"))
+# a=21
+# suma=0
+
+# for i in range(0,n+1):
+#   for t in range(0,i,a):
+#     print(a)
+#     suma=suma+a
+#     a=a+100
+#     print("Suma liczb wynosi:")
+#     print(suma)
+
+#zad10
+# from cmath import sqrt
+
+# for i in range(1,1000):
+#   if i%10==sqrt(i):
+#     print(i)
+#   elif i%100==sqrt(i):
+#     print(i)
