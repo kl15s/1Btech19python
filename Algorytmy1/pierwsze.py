4079847b2725f641024c4828e89d12e904e23edc
1B14łk <114583627+kl15s@users.noreply.github.com>
2022-11-16 13:13:54 +0000

    dodano pierwsze.py

---
+++ Algorytmy1/pierwsze.py
@@ -0,0 +1,13 @@
+#1 Sprawdzenia czy liczba jest pierwsza
+#liczba pierwsza-liczba która się dzieli tylko przez 1 i samą siebie
+#2,3,5,7,11,13,17,19,23...
+#dzielniki właściwe- dzielniki liczby poza 1 i nia samą
+
+# n=int(input())
+# for i in range(2,n):
+#   if n%i==0:
+#     print("Liczba nie jest pierwsza")
+#     exit(0)
+# print("Liczba jest pierwsza")
+
+#2 Generowanie liczb pierwszych
