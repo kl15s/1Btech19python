a8fe4653cfd9cc78d004b1b4181f4743dd254211
1B14łk <114583627+kl15s@users.noreply.github.com>
2023-01-02 11:08:41 +0000

    coś tam z KartyPracy6a.py

---
+++ Algorytmy1/KartaPracy6a.py
@@ -0,0 +1,22 @@
+#zad1
+#zad2
+a=int(input())
+if a//1==a and a//a==1:
+  print("TAK")
+else:
+  print("NIE")
+#zad3   #Liczba doskonała - jest równa sumie wszystkich swoich podzielników, mniejszych od tej liczby.
+# a=int(input())
+#zad4
+# x=int(input())
+# y=int(input())
+#zad5
+# m=int(input())
+#zad6
+# a=int(input())
+# b=int(input())
+#zad7
+# a=int(input())
+#zad8
+#zad9
+#zad10
