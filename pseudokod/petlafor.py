26468fb2992ed96b8cc0f16f5a900c9dc5120044
1B14łk <114583627+kl15s@users.noreply.github.com>
2023-01-16 11:15:16 +0000

    dodano pseudokod dla pętli for

---
+++ pseudokod/petlafor.py
@@ -0,0 +1,31 @@
+#napisz program, który oblicza sumę liczb dwucyfrowych
+# suma=0
+# for i in range(10,100):
+#   suma+=i
+#   print(suma)
+
+#PSEUDOKOD
+# suma=0
+# dla każdego i=11,12,13....19 wykonuj
+#    suma=suma+1
+# wypisz suma
+
+
+#napisz program który obliczy k liczb wpisanych przez usera
+
+# k=int(input())
+# suma=0
+
+# for i in range(k):
+#  x=int(input())
+#   suma=suma+x
+# print(suma)
+
+#PSEUDOKOD
+#czytaj k
+#suma=0
+
+# dla każdego i=1,2,3...,k wykonuj
+#    czytaj x
+#    suma=suma+x
+#wypisz suma
