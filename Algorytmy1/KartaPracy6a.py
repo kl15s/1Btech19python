d98bdbfa978289b313a5761c8e68eed41dbfcbf8
1B14łk <114583627+kl15s@users.noreply.github.com>
2023-01-16 10:41:16 +0000

    Initial commit

--- Algorytmy1/KartaPracy6a.py
+++ Algorytmy1/KartaPracy6a.py
@@ -1,22 +1,75 @@
-#zad1
-#zad2
-a=int(input())
-if a//1==a and a//a==1:
-  print("TAK")
-else:
-  print("NIE")
-#zad3   #Liczba doskonała - jest równa sumie wszystkich swoich podzielników, mniejszych od tej liczby.
+#zad 1
+# suma=0
 # a=int(input())
-#zad4
-# x=int(input())
-# y=int(input())
-#zad5
-# m=int(input())
-#zad6
+# for i in range(a):
+#    b=int(input())
+#    suma+=i
+# print(suma)
+
+#zad 2
+# n=int(input())
+# for i in range(2,n):
+#    if n % i == 0:
+# print("Liczba nie jest pierwsza")
+#    exit(0)
+# print ("Liczba jest pierwsza")
+
+#zad 3   #Liczba doskonała - jest równa sumie wszystkich swoich podzielników, mniejszych od tej liczby.
+
+#suma=0
 # a=int(input())
-# b=int(input())
-#zad7
-# a=int(input())
-#zad8
-#zad9
-#zad10
+
+#for i in range(1,a):
+#    if a % i == 0
+#    suma+=i
+# if suma==a:
+#  print("TAK")
+# else:
+#   print("NIE")
+
+#zad 4
+#a=int(input())
+#b=int(input())
+
+#while a!=b:
+#if a>b:a=a-b
+#if b>a:b=b-a
+#if a==1:
+#  print("TAK")
+#else:
+#  print("NIE")
+
+#zad 5
+#m=int(input())
+
+#lista=""
+#for i in range(10,20):
+#   x=i
+#   y=m
+#   while y!=x:
+#   if x>y:x=x-y
+#   if y>x:y=y-x 
+#   if x==1:
+#   lista+= f"{i}\n"
+# print(lista)
+
+#zad 6
+#a=int(input())
+#b=int(input())
+
+#for i in range(min(a,b),1, -1):
+#      if a % i == 0 and b % i == 0:
+#     a,b = a//i, b//i
+# print (f"{a}/{b}")
+
+#zad 7 
+#a=int(input())
+#b=int(input())
+
+#cal = a//b
+# a-= cal*b
+#for i in range(a,1,-1):
+#    if a % i == 0 ang b % i ==0:
+#    a,b= a/i, b/i
+# print(f"cal}
+#{int(a)}/{int(b)}")
