4f2571e11885eb1152deb3d7079ab04b375becb9
1B14łk <114583627+kl15s@users.noreply.github.com>
2023-01-18 13:15:20 +0000

    dodano folder listy

---
+++ listy/poczatkizlistami.py
@@ -0,0 +1,15 @@
+L=[3,7,6,1,3,3,5,4]
+
+print(L)
+print(L[1])
+print(L[6])
+print(len(L))
+
+for elem in L:
+  print(elem, end=" ")
+print("\n")
+
+for i in range(len(L)):
+  print(L[i]), end=" "
+print("\n")
+  
