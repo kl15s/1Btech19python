951bcf882407b8e1b46b309149dc1d15d420f59d
1B14łk <114583627+kl15s@users.noreply.github.com>
2023-03-01 13:14:00 +0000

    dodano nap1.py

---
+++ napisy/nap1.py
@@ -0,0 +1,43 @@
+# s=input()
+
+# for x in s:
+#   print(x)
+
+# for i in range(len(s)):
+#   print(s[i])
+
+# L=[5,7,1,4]
+# print(s,L)
+# L.sort()
+# print(s,L)
+# s.sort() -> to nie działa, błąd
+# print(s,L)
+
+#konwersja lista<-> napis
+
+# s=input()
+# L=list(s)
+# print(s,L)
+# L.sort()
+# print(s,L)
+
+
+# s=input()
+# L=list(s)
+# print(s,L)
+# L.sort()
+# s= "".join(L)
+# print(s,L)
+
+#zadania
+#sprawdź czy napis jest palindromen
+
+s=input()
+
+L=list(s)
+R=list(s)
+R.reverse()
+if L==R:
+  print("TAK")
+else:
+  print("NIE")
