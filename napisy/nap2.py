b9c5b44b51511be154d42176316950c3f9d5e81f
1B14łk <114583627+kl15s@users.noreply.github.com>
2023-03-06 11:12:18 +0000

    dodano nap2.py

---
+++ napisy/nap2.py
@@ -0,0 +1,28 @@
+#palidrom za pomocą tablicy
+
+#s=int(input())
+
+#for i in range(len(s):2):
+  #if s(#kwadratowy i)== s(#kwadratowy  len(s)-1-i):
+    #exit("NIE JEST")
+ # exit("JEST PALINDROMEM")
+
+#anagram za pomocą funkcji
+
+#a=int(input())
+#b=int(input())
+#A=list(a)
+#B=list(b)
+#A.sort()
+#B.sort()
+
+#a="".join(A)
+#b="".join(B)
+#print(a,b)
+#if a==b:
+  #print("TAK")
+#else:
+  #print("NIE")
+
+
+    
